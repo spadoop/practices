cfe65e3c271e3387747f0d0b50c0f6da6cfb1d7b
spadoop <6165218@qq.com>
2016-12-15 21:05:43 +0800

    Levenshtein编辑距离算法

--- httpclient/src/main/java/org/apache/http/LevenshteinDistanceMetric.java
+++ httpclient/src/main/java/org/apache/http/LevenshteinDistanceMetric.java
@@ -1,6 +1,6 @@
 package org.apache.http;
 
-import edu.emory.mathcs.backport.java.util.Arrays;
+import java.util.Arrays;
 
 public class LevenshteinDistanceMetric  {
 	/**
@@ -11,25 +11,25 @@ public class LevenshteinDistanceMetric  {
     	long start = System.nanoTime();
 		int m = x.length();
 		int n = y.length();
-		int[][] T = new int[2][32];
+		int[][] T = new int[2][20];
 		T[0][0] = 0;
-		for (int j = 0; j < n; j++) {
+		for (int j = 0; j < m; j++) {
 			T[0][j + 1] = T[0][j] + 1;
 		}
-		for (int i = 0; i < m; i++) {
+		for (int i = 0; i < n; i++) {
 //			T[i+ 1][0] = T[i][0] +1;
-			for (int j =  0; j <= ( n-1 ); j++) {
+			for (int j =  0; j <= ( m-1 ); j++) {
 			//for (int j = i-1>0?i-1:0; j <= (i+1>n-1?n-1:i+1); j++) {
-				T[  1][j + 1] = min(T[0][j] + sub(x, i, y, j), T[0][j + 1]
+				T[  1][j + 1] = min(T[0][j] + sub(x, j, y, i), T[0][j + 1]
 						+ 1, T[ 1][j] + 1);
 			}
-			System.out.println(Arrays.toString(T[0] ));
+//			System.out.println(Arrays.toString(T[0] ));
 			System.out.println(Arrays.toString(T[1] )); 
 			System.arraycopy(T[1], 0, T[0], 0, T[1].length);
-			System.out.println(" ");
+//			System.out.println(" ");
 		}
     	System.out.println(System.nanoTime()-start);
-		return T[1][n];
+		return T[1][m];
 	}
 
 	private int sub(String x, int xi, String y, int yi) {
@@ -50,6 +50,6 @@ public class LevenshteinDistanceMetric  {
 	
 	public static void main(String...s){
 		LevenshteinDistanceMetric ldm = new LevenshteinDistanceMetric();
-		System.out.println(ldm.distance("azzelity163134pzs", "asfsdfa"));
+		System.out.println(ldm.distance("azzelityasfsdfazs", "asfsdfa"));
 	}
 }
